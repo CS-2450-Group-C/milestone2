5f95436066d9accb07b33c3a30ae2de9816fbb8d
Rowley <54674066+drowley8@users.noreply.github.com>
2023-02-05 19:23:30 -0700

    add main

---
+++ main.py
@@ -0,0 +1,29 @@
+from uvsim import Machine
+import sys
+from parse import parse
+
+def main():
+    # Ensure file is given
+    if len(sys.argv) < 2:
+        print("Usage: python main.py fileName.txt")
+        return
+
+    # Fill memory from given file
+    memory = []
+    try:
+        memory = parse(sys.argv[1])
+    except ValueError as ex:
+        print(ex)
+        return
+    
+    # Create new machine from parsed memory
+    machine = Machine(memory)
+
+    # uncomment when ready
+    # while (machine.running):
+    #     machine.tick()
+
+    
+
+if __name__ == "__main__":
+    main()
