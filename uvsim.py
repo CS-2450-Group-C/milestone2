6ea579d9ebbaedcd48b7dcf18e833f6c39a7b366
Rowley <54674066+drowley8@users.noreply.github.com>
2023-02-05 19:24:02 -0700

    create Machine Class

--- uvsim.py
+++ uvsim.py
@@ -1,21 +1,8 @@
-import sys
-from parse import parse
 
-def main():
-    # Ensure file is given
-    if len(sys.argv) < 2:
-        print("Usage: python uvsim.py fileName.txt")
-        return
+class Machine:
+    def __init__(self, init_mem):
+        self._memory = [0000] * 100
+        for i, value in enumerate(init_mem):
+            self._memory[i] = value
+        print(self._memory)
 
-    # Fill memory from given file
-    memory = []
-    try:
-        memory = parse(sys.argv[1])
-    except ValueError as ex:
-        print(ex)
-        return
-
-
-
-if __name__ == "__main__":
-    main()
