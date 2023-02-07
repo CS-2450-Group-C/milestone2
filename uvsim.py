37d14094708c0c29f6546c9affb47d815de4da0d
hyrum-c <hyrum.cox@gmail.com>
2023-02-06 17:03:15 -0700

    Added control functions

--- uvsim.py
+++ uvsim.py
@@ -1,8 +1,22 @@
 
 class Machine:
     def __init__(self, init_mem):
+        self.accumulator = 0000
         self._memory = [0000] * 100
         for i, value in enumerate(init_mem):
             self._memory[i] = value
         print(self._memory)
+    
+    # Should the functions just be class methods?\
+    
+    # def read(self, memory_index):
+    #     # Read takes user input and stores that in a location in memory
+    #     new_word = input("Enter a new four-digit word. Ex: +2156, -4328: ")
+    #     self.memory[memory_index] = self.accumulator
+
+    # def store(self, memory_index):
+    #     # Store what is in the accumulator into a location in memory
+
+    #     # Psuedo code for storing in memory
+    #     self.memory[memory_index] = accumulator
 
