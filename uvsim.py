2e75e83e0589609b4e4d012279e57338b10d616c
hyrum-c <hyrum.cox@gmail.com>
2023-02-06 19:03:40 -0700

    Added pc

--- uvsim.py
+++ uvsim.py
@@ -1,22 +1,31 @@
 
 class Machine:
     def __init__(self, init_mem):
-        self.accumulator = 0000
+        self._accumulator = 0000
+        self._program_counter = 0
         self._memory = [0000] * 100
         for i, value in enumerate(init_mem):
             self._memory[i] = value
         print(self._memory)
-    
-    # Should the functions just be class methods?\
-    
-    # def read(self, memory_index):
-    #     # Read takes user input and stores that in a location in memory
-    #     new_word = input("Enter a new four-digit word. Ex: +2156, -4328: ")
-    #     self.memory[memory_index] = self.accumulator
 
-    # def store(self, memory_index):
-    #     # Store what is in the accumulator into a location in memory
+    def tick(pc_value = None):
+        # 
+        pass
+    
+    def interpret_instruction(instruction):
+        # 
+        pass
+    
+    # Should the functions just be class methods?
 
-    #     # Psuedo code for storing in memory
-    #     self.memory[memory_index] = accumulator
+    def read(self, memory_index):
+        # Read takes user input and stores that in a location in memory
+        new_word = input("Enter a new four-digit word. Ex: +2156, -4328: ")
+        self.memory[memory_index] = self.accumulator
+
+    def store(self, memory_index):
+        # Store what is in the accumulator into a location in memory
+
+        # Psuedo code for storing in memory
+        self.memory[memory_index] = accumulator
 
